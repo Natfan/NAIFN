796a22e2b69352365dfeb03562d40d13586a42e5
ryankrage77 <ryankrage77@gmail.com>
2017-04-11 12:18:29 +0100

    made the pseudo-code in python

---
+++ NAIFN.py
@@ -0,0 +1,20 @@
+#NAIFN
+#Takes a text input, outputs text & speech
+#uses regex for language parsing
+import os
+
+#function that cocatonates text output with say command
+def speak(answer):
+	spokenAnswer = ("say " + answer)
+
+#input parser. Figures out appropiate response to input
+def parse():
+	#In progress
+
+#Takes input from user, tidies it up for parsing (fixing typos, etc)
+def input():
+	#In progress
+
+#I think you can guess what this one does
+def main():
+	#In progress
