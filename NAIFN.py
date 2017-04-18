525129da370ac72da74a3dcbf4b1143e664dd226
ryankrage77 <ryankrage77@gmail.com>
2017-04-18 12:59:16 +0100

    Attempted the beginnings of a command parser

--- NAIFN.py
+++ NAIFN.py
@@ -6,28 +6,27 @@ import os
 
 #function that cocatonates text output with say command
 def speak(answer):
-	print(answer)
 	spokenAnswer = ("say " + answer)
 	os.system(spokenAnswer)
+	print(answer)
 
 #Input parser. Figures out appropiate response to input using regex
 def parse():
-	#compare each word to a list of known words
-	#Choose the output that best corresponds
-	#For example, if input includes 'say' && 'funny':
-	#	output = [say something funny command]
-	#if input includes 'tell' && 'joke':
-	#	output = [tell joke command]
-	#if input includes 'weather':
-	#	output = [weather command]
-	#and so on and so forth.
+	if any(command in userInput for command in commandList):
+    	answer = "Command Found."
+    else:
+    	answer = "No command Found."
 
 #Takes input from user, tidies it up for parsing (fixing typos, etc)
 def input():
+	userInput = input("> ")
 	#compare each word of input to a dictionary
 	#auto-correct typos (need auto-correct module)
-	#If question inludes 'does/why/where/when/how/who' append a question mark
+	#If question includes 'does/why/where/when/what/how/who' append a question mark
 
 #I think you can guess what this one does
 def main():
-	#In progress
+	while quit = False:
+		speak(parse(input(userInput)))
+
+main()
